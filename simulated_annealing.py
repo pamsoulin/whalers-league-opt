abb43bbd984135f7352740e059dbc28533b077d5
pamsoulin <pamsoulin@gmail.com>
2025-06-15 08:07:25 -0700

    added plotting to simulated_annealing, experimented with cooling schedules + scaling

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,30 +1,92 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display
 
 def metropolis_acceptance_prob(dy, temp):
     if dy <= 0:
         return np.float64(1.0)
     else:
+        #print(np.exp(-dy/temp))
         return np.exp(-dy/temp)
     
 def generate_exponential_annealing_schedule(gamma, t_init):
-    return lambda t: np.power(gamma, t) * t_init
+    return lambda k: np.power(gamma, k) * t_init
+
+def generate_logarithmic_annealing_schedule(t_init):
+    return lambda k: (t_init * np.log(2))/np.log(k+1) if k > 1 else t_init
 
 # implements simulated annealing to minimize the function f
-def simulated_annealing(f, x, T, t, k_max):
+def simulated_annealing(f, x, T, t, k_max, avg_interval = -1, plot_interval=-1):
+
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(3, 1, 1)
+    ax2 = fig.add_subplot(3, 1, 2)
+    ax3 = fig.add_subplot(3, 1, 3)
+
+    ys = []
+    acc_probs = []
+    temps = []
+
+    y_avgs = []
+    y_sum = 0
+    acc_prob_avgs = []
+    acc_prob_sum = 0
+    avg_xs = []
+
     x_current = x
     y_current = f(x_current)
     x_best, y_best = x_current, y_current
     for k in range(k_max):
-        x_new = T(x)
+        x_new = T(x_current)
         y_new = f(x_new)
         dy = y_new - y_current
+        temp = t(k)
+        acceptance_prob = metropolis_acceptance_prob(dy, temp)
         #todo: add way to seed RNG
-        if dy <= 0 or np.random.random() < metropolis_acceptance_prob(dy, t(k)):
+        if dy <= 0 or np.random.random() < acceptance_prob:
             x_current, y_current = x_new, y_new
+            #print("Took swap")
         if y_new < y_best:
             x_best, y_best = x_new, y_new
-        if k%50 == 0:
-            print(f"k = {k}")
-            print(x_best)
-            print(y_best)
+        ys.append(y_current)
+        acc_probs.append(acceptance_prob)
+        temps.append(temp)
+        if k>0 and plot_interval>-1:
+            if avg_interval > -1:
+                # print(k)
+                # print(y_current)
+                # print(y_sum)
+                y_sum += y_current
+                acc_prob_sum += acceptance_prob
+                if k%avg_interval == 0:
+                    y_avgs.append(y_sum/avg_interval)
+                    # print(y_sum)
+                    # print(avg_interval)
+                    # print(y_sum/avg_interval)
+                    # print(y_avgs[-1])
+                    # print(type(y_avgs[-1]))
+                    acc_prob_avgs.append(acc_prob_sum/avg_interval)
+                    avg_xs = [((i+1)*avg_interval)-(avg_interval//2) for i in range(k//avg_interval)]
+                    y_sum, acc_prob_sum = 0, 0
+            if k%plot_interval == 0:
+                display.clear_output(wait = True)
+                ax1.cla()
+                ax2.cla()
+                ax3.cla()
+                print(f"k = {k}")
+                print(f"y_best: {y_best}")
+                print(f"y_current: {y_current}")
+                ax1.plot(ys)
+                ax2.scatter(range(k+1), acc_probs, alpha = 0.5)
+                ax3.plot(temps[1:])
+
+                if avg_interval > -1:
+                    # print(avg_xs)
+                    # print(y_avgs)
+                    ax1.plot(avg_xs, y_avgs, color='red')
+                    ax2.plot(avg_xs, acc_prob_avgs, color='red')
+
+                display.display(fig)
+            
+                    
     return x_best
--- team.py
+++ team.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+POSITION_LIMITS = {'C':2, 'F':4, 'G':4}
+
 class Selection:
     def __init__(self, selection_df: pd.DataFrame, full_roster_df: pd.DataFrame) -> None:
         self.selection_df = selection_df
@@ -8,12 +10,24 @@ class Selection:
 
     def __str__(self):
         return str(self.selection_df)
+    
+    def copy(self):
+        return Selection(self.selection_df, self.full_roster_df)
 
     def total_fp(self):
         return self.selection_df['fp'].sum()
 
     def total_dv(self):
         return self.selection_df['dv'].sum()
+    
+    def is_valid(self):
+        position_counts = self.selection_df.value_counts('position')
+        #write so that a missing position doesn't cause a key error
+        valid_positions = position_counts['C'] == 2 and position_counts['F'] == 4 and position_counts['G'] == 4
+        team_counts = self.selection_df.value_counts('team')
+        valid_teams = sum([1 if count > 2 else 0 for count in team_counts.values]) == 0
+        valid_dv = self.total_dv() <= 100.0
+        return valid_positions and valid_teams and valid_dv
 
     def possible_swaps(self):
         all_teams = self.full_roster_df['team'].unique()
@@ -25,7 +39,7 @@ class Selection:
         for _, player_data in self.selection_df.iterrows():
             swap_position = player_data.loc['position']
             swap_teams = [team for team in all_teams if team == player_data.loc['team'] or not team in maxed_teams]
-            swap_budget = 100.0 - current_dv + player_data.loc['fp']
+            swap_budget = 100.0 - current_dv + player_data.loc['dv']
             alternate_players_mask = \
                 (available_players_df['team'].isin(swap_teams)) & \
                 (available_players_df['dv'] <= swap_budget) & \
@@ -33,14 +47,35 @@ class Selection:
             alternate_players_df = available_players_df[alternate_players_mask]
             swap_list = [(player_data.loc['name'], alternate_player_data.loc['name']) for _, alternate_player_data in alternate_players_df.iterrows()]
             all_swaps.extend(swap_list)
-        #print(all_swaps)
         return(np.array(all_swaps))
+    
+    def possible_additions(self):
+        available_players_df = self.full_roster_df[~self.full_roster_df['id'].isin(self.selection_df['id'])]
+        all_teams = self.full_roster_df['team'].unique()
+        team_counts = self.selection_df.value_counts('team')
+        maxed_teams = [team for team, count in team_counts.items() if count == 2]
+        possible_teams = [team for team in all_teams if team not in maxed_teams]
+        possible_budget = 100.0 - self.total_dv()
 
+        position_counts = self.selection_df.value_counts['position']
+        possible_positions = [pos for pos in ['C','F','G']]
+        possible_players_mask = \
+            (available_players_df['team'].isin(possible_teams)) & \
+            (available_players_df['dv'] <= possible_budget) & \
+            (available_players_df['position'] == swap_position)
+        
+
+
+    def add_player(self, name):
+        new_player_data = self.full_roster_df[self.full_roster_df['name'] == name]
+        #add exception if player isn't in the full roster?
+        self.selection_df = pd.concat([self.selection_df, new_player_data])
+
+    def remove_player(self, name):
+        self.selection_df = self.selection_df[~(self.selection_df['name'] == name)]
 
     def swap_players(self, swap):
-        #old_player_data = self.full_roster_df[self.full_roster_df['name'] == swap[0]]
-        new_player_data = self.full_roster_df[self.full_roster_df['name'] == swap[1]]
-        # print(new_player_data)
-        # print(self.selection_df)
-        new_selection_df = pd.concat([self.selection_df[~(self.selection_df['name']==swap[0])], new_player_data])
-        return Selection(new_selection_df, self.full_roster_df)
+        self.remove_player(swap[0])
+        self.add_player(swap[1])
+
+    
